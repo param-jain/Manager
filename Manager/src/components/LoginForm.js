2618b4ca4a9a0e1245614e3272e76b8a479c5ffc
Param Jain <param_jain@icloud.com>
2018-09-19 11:45:39 +0530

    Login & Authentication Part Done
    Created a Login Screen using Redux Library.
    Used redux-thunk for AJAX i.e. using MiddleWare for React Native Asynchronous JS Calls.
    Need to install redux-thunk in your root directory.
    Completed Authentication part with firebase.

--- Manager/src/components/LoginForm.js
+++ Manager/src/components/LoginForm.js
@@ -1,12 +1,46 @@
 import React, { Component } from 'react';
-import { Card, CardSection, Input, Button } from './common';
+import { View, Text } from 'react-native';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 import { connect } from 'react-redux';
-import { emailChanged } from '../actions';
+import { emailChanged, passwordChanged, loginUser } from '../actions';
 
 class LoginForm extends Component {
 
     onEmailChange(text) {
         this.props.emailChanged(text);
+    }
+
+    onPasswordChange(text) {
+        this.props.passwordChanged(text);
+    }
+
+    onButtonPress() {
+        const { email, password } = this.props;
+        this.props.loginUser({ email, password });
+    }
+
+    renderError() {
+        if(this.props.error) {
+            return (
+                <View style={{ backgroundColor: 'white' }}>
+                    <Text style={styles.errorTextStyle}>
+                        {this.props.error}
+                    </Text>
+                </View>
+            );
+        }
+    }
+
+    renderButton() {
+        if(this.props.loading) {
+            return <Spinner size="large" />;
+        }
+
+        return (
+            <Button onPress={this.onButtonPress.bind(this)}>
+                Login
+            </Button>
+        );
     }
 
     render() {
@@ -17,6 +51,7 @@ class LoginForm extends Component {
                         label="Email"
                         placeholder="email@domain.com"
                         onChangeText={this.onEmailChange.bind(this)}
+                        value={this.props.email}
                     />
                 </CardSection>
 
@@ -25,17 +60,38 @@ class LoginForm extends Component {
                         secureTextEntry
                         label="Password"
                         placeholder="password"
+                        onChangeText={this.onPasswordChange.bind(this)}
+                        value={this.props.password}
                     />
                 </CardSection>
 
+                {this.renderError()}
+
                 <CardSection>
-                    <Button>
-                        Login
-                    </Button>
+                    {this.renderButton()}
                 </CardSection>
             </Card>
         );
     }
 }
 
-export default connect(null, { emailChanged })(LoginForm);
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red'
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        email: state.auth.email,
+        password: state.auth.password,
+        error: state.auth.error,
+        loading: state.auth.loading
+    };
+};
+
+export default connect(mapStateToProps, {
+        emailChanged, passwordChanged, loginUser
+    })(LoginForm);
